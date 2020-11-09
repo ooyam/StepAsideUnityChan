7b114546e89caa5c65906bd8fffeb9c4cbb2c7ee
ooyam <73095407+ooyam@users.noreply.github.com>
2020-11-09 17:21:51 +0900

    Initial commit

---
+++ Assets/CoinController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinController : MonoBehaviour
+{
+
+    //メインカメラを入れる
+    public GameObject mainCamObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        //回転を開始する角度を設定
+        this.transform.Rotate(0, Random.Range(0, 360), 0);
+        //メインカメラの取得
+        mainCamObj = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //回転
+        this.transform.Rotate(0, 3, 0);
+
+        //メインカメラがオブジェクトを通過した場合
+        if (mainCamObj.transform.position.z > this.transform.position.z)
+        {
+            //通過したオブジェクトを破棄
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/ItemController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    //メインカメラを入れる
+    public GameObject mainCamObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //メインカメラの取得
+        mainCamObj = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //メインカメラがオブジェクトを通過した場合
+        if (mainCamObj.transform.position.z > this.transform.position.z)
+        {
+            //通過したオブジェクトを破棄
+            Destroy(this.gameObject);
+        }
+    }
+}
